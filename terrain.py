f85a1cd01e1a453e11a4e01ecfed5c9f02ddc6de
Jason Buckner <jason@jasonbuckner.com>
2013-02-06 22:49:06 -0800

    yet another big rewrite, this time breaking down a specific range of coordinates instead of breaking it up by tile… this way is better, but I have a few things to work out with it still, particularly limiting resolution to the available grid

--- terrain.py
+++ terrain.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 
-import Image
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -10,23 +9,34 @@ from pylab import *
 
 from srtm import SRTMDownloader
 
+# SF
+# ne_corner = {
+#     'lat': 37.836903,
+#     'lng': -122.35611
+# }
+# sw_corner = {
+#     'lat': 37.704467,
+#     'lng': -122.520905
+# }
+
+# Marin
 sw_corner = {
-    'lat': 36,
-    'lng': -123
+    'lat': 37.802275,
+    'lng': -122.558816
 }
 ne_corner = {
-    'lat': 38,
-    'lng': -121
+    'lat': 37.877107,
+    'lng': -122.477105
 }
 
 tile_count_lat = abs(sw_corner['lat'] - ne_corner['lat'])
 tile_count_lng = abs(sw_corner['lng'] - ne_corner['lng'])
 
-resolution = 99  # numer of samples we take of each hgt in each direction
+resolution = 750  # numer of samples we take of each hgt in each direction
 pixels = resolution * resolution  # for calculating percent complete
 divisor = resolution + 1.0  # for calculating the point to sample from
 
-srtm_format = 3  # 3, 1
+srtm_format = 1  # 3, 1
 
 dpi = 100
 width = 40  # inches
@@ -38,67 +48,10 @@ output_resolution = {
 img_directory = "images/srtm%s" % srtm_format  # where to save the images
 
 
-def merge_images():
-    # images = os.listdir(img_directory)
-
-    stitched_dimensions = {
-        "x": output_resolution["x"] * tile_count_lng,
-        "y": output_resolution["y"] * tile_count_lat
-    }
-
-    print stitched_dimensions
-
-    blank_image = Image.new("RGBA",
-                            (stitched_dimensions["x"],
-                                stitched_dimensions["y"]),
-                            "black")
-
-    for lat_i, lat in enumerate(range(sw_corner['lat'], ne_corner['lat'])):
-        for lng_i, lng in enumerate(range(sw_corner['lng'], ne_corner['lng'])):
-            filename = "%sx%s.png" % (lat, lng)
-            try:
-                filepath = os.path.join(img_directory, filename)
-                im = Image.open(filepath)
-                print filename, im.format, "%dx%d" % im.size, im.mode
-                blank_image.paste(im, (2000 * lng_i, 2000 * lat_i))
-            except IOError:
-                pass
-    blank_image.save("merged_file.png")
-
-
 def update_status(percent):
     sys.stdout.write("\r%3d%%" % percent)
     sys.stdout.flush()
 
-
-def parse_tile(tile):
-    outfile = zeros((resolution, resolution))
-    valley = {"lat": None, "lng": None, "alt": 32767}
-    peak = {"lat": None, "lng": None, "alt": 0}
-    c = 0  # just a counter to track completion
-
-    print "Processing: %s, %s" % (tile.lat, tile.lon)
-    for lat_i in range(0, resolution):
-        for lng_i in range(0, resolution):
-            lat_point = tile.lat + (lat_i / divisor)
-            lng_point = tile.lon + (lng_i / divisor)
-            alt = tile.getAltitudeFromLatLon(lat_point, lng_point)
-            if alt:
-                outfile[lat_i][lng_i] = float(alt)
-                if alt < valley["alt"]:
-                    valley["alt"] = alt
-                    valley["lat"] = lat_point
-                    valley["lng"] = lng_point
-                if alt > peak["alt"]:
-                    peak["alt"] = alt
-                    peak["lat"] = lat_point
-                    peak["lng"] = lng_point
-            c = c + 1.0
-            if (c % 1000) == 0:
-                percent_complete = (c / pixels) * 100.0
-                update_status(percent_complete)
-    print "\n"
-    return outfile, valley, peak
 
 if __name__ == '__main__':
     fig = plt.figure(frameon=False)
@@ -125,7 +78,6 @@ if __name__ == '__main__':
 
     sample_resolution_lat = tile_count_lat * resolution
     sample_resolution_lng = tile_count_lng * resolution
-    total_sample_resolution = sample_resolution_lat * sample_resolution_lng
 
     tile = None
     valley = {"lat": None, "lng": None, "alt": 32767}
@@ -141,54 +93,78 @@ if __name__ == '__main__':
             reprocess = False
 
     if reprocess:
-        outfile = zeros((sample_resolution_lat, sample_resolution_lng))
-        for lat_i in range(0, sample_resolution_lat):
-            for lng_i in range(0, sample_resolution_lng):
-                lat_point = sw_corner["lat"] + (lat_i / divisor)
-                lng_point = sw_corner["lng"] + (lng_i / divisor)
-                tile_lat = floor(lat_point)
-                tile_lng = floor(lng_point)
+        lng_delta = abs(ne_corner['lng'] - sw_corner['lng'])
+        lat_delta = abs(ne_corner['lat'] - sw_corner['lat'])
+        aspect_ratio = lng_delta / lat_delta
+        lng_sample_points = int(resolution * aspect_ratio)
+        lat_sample_points = int(resolution)
+
+        lng_interval = lng_delta / lng_sample_points * 1.0
+        lat_interval = lat_delta / lat_sample_points * 1.0
+
+        total_sample_resolution = lng_sample_points * lat_sample_points
+
+        print aspect_ratio, lng_delta, lat_delta, lng_interval, lat_interval, lng_sample_points, lat_sample_points, total_sample_resolution
+
+        outfile = zeros((lat_sample_points, lng_sample_points))
+
+        check_point = sw_corner.copy()
+        for lat_i in range(0, lat_sample_points):
+            for lng_i in range(0, lng_sample_points):
+                tile_lat = int(math.floor(check_point['lat']))
+                tile_lng = int(math.floor(check_point['lng']))
                 if not tile or tile.lat != tile_lat or tile.lon != tile_lng:
                     tile = downloader.getTile(tile_lat, tile_lng)
-                alt = tile.getAltitudeFromLatLon(lat_point, lng_point)
+                alt = tile.getAltitudeFromLatLon(check_point["lat"],
+                                                 check_point["lng"])
                 if alt:
-                    outfile[lat_i][lng_i] = float(alt)
+                    outfile[(lat_sample_points - lat_i) - 1][lng_i] = float(alt)
                     if alt < valley["alt"]:
                         valley["alt"] = alt
-                        valley["lat"] = lat_point
-                        valley["lng"] = lng_point
+                        valley["lat"] = check_point["lat"]
+                        valley["lng"] = check_point["lng"]
                     if alt > peak["alt"]:
                         peak["alt"] = alt
-                        peak["lat"] = lat_point
-                        peak["lng"] = lng_point
-                c = c + 1.0
+                        peak["lat"] = check_point["lat"]
+                        peak["lng"] = check_point["lng"]
+
+                check_point["lng"] += lng_interval
+                if check_point["lng"] > ne_corner["lng"]:
+                    check_point["lng"] = sw_corner["lng"]
+                    check_point["lat"] = check_point["lat"] + lat_interval
+                c += 1.0
                 if (c % 1000) == 0:
                     percent_complete = (c / total_sample_resolution) * 100.0
                     update_status(percent_complete)
+
         np.save(cache_file, outfile)
 
-        # compress the color range of the output
-        # color_scale = np.log1p(outfile)
-        color_compressed = zeros((sample_resolution_lat,
-                                  sample_resolution_lng))
-        grey_max = 200
-        grey_min = 100
-        for (x, y), value in np.ndenumerate(outfile):
-            # compress colors, found here: http://stackoverflow.com/a/929107
-            color_compressed[x][y] = ((
-                (value - valley["alt"]) * (grey_max - grey_min)) /
-                (peak["alt"] - valley["alt"])) + grey_min
+        print peak, valley
     else:
         outfile = np.load(cache_file)
+
+    # compress the color range of the output
+    color_scale = np.log1p(outfile)
+    # color_compressed = zeros((lat_sample_points,
+    #                          lng_sample_points))
+    # grey_max = 200
+    # grey_min = 100
+    # for (x, y), value in np.ndenumerate(outfile):
+    #     # compress colors, found here: http://stackoverflow.com/a/929107
+    #     color_compressed[x][y] = ((
+    #         (value - valley["alt"]) * (grey_max - grey_min)) /
+    #         (peak["alt"] - valley["alt"])) + grey_min
 
     #for vmin in range(-100, 200, 100):
     #    for vmax in range(1000, 2500, 500):
     vmin = None
     vmax = None
-    ax.imshow(outfile, aspect='normal', interpolation='bilinear',
-              cmap=cm.gray, alpha=1.0, norm=None, vmin=vmin, vmax=vmax)
-    filename = "%s,%s_%s,%s_%s_%s_%s" % (
+    colormaps = [m for m in cm.datad if not m.endswith("_r")]
+    colormap = cm.gray
+    print "colormap: %s" % colormap
+    ax.imshow(color_scale, aspect='normal', interpolation='bilinear',
+              cmap=colormap, alpha=1.0)
+    filename = "%s,%s_%s,%s_%s_%s.png" % (
         sw_corner["lat"], sw_corner["lng"], ne_corner["lat"],
-        ne_corner["lng"], resolution, vmin, vmax)
-    print "processing vmin: %s, vmax: %s" % (vmin, vmax)
+        ne_corner["lng"], resolution, colormap.name)
     fig.savefig("images/merged/%s" % filename)
